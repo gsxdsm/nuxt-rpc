670a76790a493ee66e6b158cf1b808520065bc05
gsxdsm <gsxdsm@users.noreply.github.com>
2024-12-05 19:57:40 -0800

    Update app.vue

--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useAsyncData } from '#app'
-import { hello } from './hello'
+import { hello } from '~/server/rpc/hello'
 
 const { data } = await useAsyncData('ping', () => hello({ name: 'Evan' }))
 </script>
