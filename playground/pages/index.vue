ef799b40bce30625c54ea6bbfb0162e0725566ab
Christopher Lang <eclipxe@gmail.com>
2024-12-08 12:19:12 -0800

    Fix tsconfig path and refactor index.vue for consistency; improve formatting and structure

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,51 +1,46 @@
 <script setup lang="ts">
-import { useAsyncData } from '#app'
+import { useAsyncData } from '#app';
 
-const { data: todos, refresh } = await useAsyncData('todos', () => rpc.todo.getTodos())
+const { data: todos, refresh } = await useAsyncData('todos', () =>
+  rpc.todo.getTodos()
+);
 const todoClient = rpcClient({
-  fetchOptions:{
-    headers:{
-      "Authorization": "Bearer token"
-    },
-    onRequest() {
-      console.log('request')
-    }
-  }
+  headers: {
+    Authorization: 'Bearer token',
+  },
+  onRequest() {
+    console.log('request');
+  },
 }).todo;
 
-async function handleChange (id: number) {
-  await rpc.todo.toggleTodo(id)
-  await refresh()
+async function handleChange(id: number) {
+  await rpc.todo.toggleTodo(id);
+  await refresh();
 }
 
-async function handleDelete (id: number) {
-  await todoClient.deleteTodo(id)
-  await refresh()
+async function handleDelete(id: number) {
+  await todoClient.deleteTodo(id);
+  await refresh();
 }
 </script>
 
 <template>
   <div>
     <ul>
-      <li
-        v-for="todo in todos"
-        :key="todo.id"
-      >
+      <li v-for="todo in todos" :key="todo.id">
         <h2>
           <span>{{ todo.title }}</span>
           <input
             type="checkbox"
             :checked="todo.completed"
             @change="handleChange(todo.id)"
-          >
-          <button @click="handleDelete(todo.id)">
-            remove
-          </button>
+          />
+          <button @click="handleDelete(todo.id)">remove</button>
         </h2>
         <p>
           <span
             :style="{
-              textDecoration: todo.completed ? 'line-through' : undefined
+              textDecoration: todo.completed ? 'line-through' : undefined,
             }"
           >
             {{ todo.content }}
@@ -54,13 +49,13 @@ async function handleDelete (id: number) {
         </p>
       </li>
     </ul>
-    <hr>
+    <hr />
     <TodoForm @create="refresh" />
   </div>
 </template>
 
 <style>
-input[type="checkbox"] {
+input[type='checkbox'] {
   cursor: pointer;
   margin: 13;
 }
